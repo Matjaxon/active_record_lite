3876ec52c09c24086dcd6efb68d7e6efa52a71ea
Matjaxon <MattRJackson1@gmail.com>
2016-07-30 13:06:26 -0700

    Partially commpleted associatable section

--- skeleton/lib/03_associatable.rb
+++ skeleton/lib/03_associatable.rb
@@ -1,5 +1,6 @@
 require_relative '02_searchable'
 require 'active_support/inflector'
+# require 'byebug'
 
 # Phase IIIa
 class AssocOptions
@@ -10,30 +11,57 @@ class AssocOptions
   )
 
   def model_class
-    # ...
+    Object::const_get(@name.classify)  #Finds the existing class if it exsits.
+  end
+
+  def foreign_key
+    foreign_key_string = ActiveSupport::Inflector.underscore(@name)
+    @options[:foreign_key] ||= "#{foreign_key_string}_id".to_sym
+  end
+
+  def primary_key
+    @options[:primary_key] ||= "id".to_sym
+  end
+
+  def class_name
+    class_name_string = ActiveSupport::Inflector.classify(@name)
+    @options[:class_name] ||= class_name_string
   end
 
   def table_name
-    # ...
+    model_class.table_name ||= ActiveSupport::Inflector.tableize(@name)
   end
 end
 
 class BelongsToOptions < AssocOptions
   def initialize(name, options = {})
-    # ...
+    @name = name
+    @options = options
   end
 end
 
 class HasManyOptions < AssocOptions
   def initialize(name, self_class_name, options = {})
-    # ...
+    self_class_options = BelongsToOptions.new(self_class_name, options)
+
+    @name = name
+    @options = options
+
+    @options[:primary_key] ||= self_class_options.primary_key
+    @options[:foreign_key] ||= self_class_options.foreign_key
   end
 end
 
 module Associatable
   # Phase IIIb
   def belongs_to(name, options = {})
-    # ...
+    options = BelongsToOptions.new(name, options)
+    p options
+    p options.model_class
+    # p self
+    # define_method "#{name}" do
+    #   options.class_name.model_class.where()
+    # end
   end
 
   def has_many(name, options = {})
@@ -46,5 +74,6 @@ module Associatable
 end
 
 class SQLObject
+  extend Associatable
   # Mixin Associatable here...
 end
